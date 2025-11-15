4f0e15e7525a902d3d9ab26d30df3547169c8a50
mnagoju <mnagoju@cotractor.ea.com>
2025-11-15 19:30:34 +0530

    first commit

---
+++ src/main/java/com/cloudeagle/cloudeagledropsync/CloudeagleDropsyncApplication.java
@@ -0,0 +1,13 @@
+package com.cloudeagle.cloudeagledropsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudeagleDropsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudeagleDropsyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloudeagle/cloudeagledropsync/CloudeagleDropsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudeagle.cloudeagledropsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudeagleDropsyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
